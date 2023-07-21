7d5619958eaf5a554db146a0426392ce25949fe8
Your Name <you@example.com>
2023-07-21 16:44:16 +0700

    Authentication: remove console.log

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -7,7 +7,6 @@ fs.readFile(fileList, function(err, data) {
     if(err) throw err;
     data = data.toString();
     // data = data.replace(/^\.(.+)/gm, 'myString$1');
-    console.log(data)
     fs.writeFile(fileListBak, data, function(err) {
         err || console.log('Data replaced \n', data);
     });
