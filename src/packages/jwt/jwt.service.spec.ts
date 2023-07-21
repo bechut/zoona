21acbd6ee77f53ad4ca528a3c92ba2e4a8d8d826
Your Name <you@example.com>
2023-07-21 10:12:49 +0700

    Setup #3: jwt, config module, momentjs

---
+++ src/packages/jwt/jwt.service.spec.ts
@@ -0,0 +1,21 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { JwtService } from './jwt.service';
+import { JwtModule } from '@nestjs/jwt';
+
+describe('JwtService', () => {
+  let service: JwtService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [JwtModule],
+      providers: [JwtService],
+    }).compile();
+
+    service = module.get<JwtService>(JwtService);
+  });
+
+  it('should be defined', () => {
+    service.get();
+    expect(service.get).toBeDefined();
+  });
+});
